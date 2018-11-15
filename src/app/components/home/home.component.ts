654609acdaf4afc3f4834e9a30dfeaeee0f7c9a2
dcocking <dustin.cocking@gmail.com>
2018-11-14 23:56:40 -0500

    Cleaning up home component and scss

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -8,51 +8,51 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 
-  animations: [
+  // animations: [
 
-    trigger('openClose', [
+  //   trigger('openClose', [
 
-      transition(':enter', [
-        style({ opacity: 0, height: 0 }),
-        animate('300ms ease-in-out', style({ opacity: 1, height: 200 })),
-      ]),
+  //     transition(':enter', [
+  //       style({ opacity: 0, height: 0 }),
+  //       animate('300ms ease-in-out', style({ opacity: 1, height: 200 })),
+  //     ]),
 
-      transition(':leave', [
-        style({ opacity: '*', height: '*' }),
-        animate('300ms ease-in-out', style({ opacity: 0, height: 0 })),
-      ])
-    ]),
+  //     transition(':leave', [
+  //       style({ opacity: '*', height: '*' }),
+  //       animate('300ms ease-in-out', style({ opacity: 0, height: 0 })),
+  //     ])
+  //   ]),
 
-    trigger('slideIn', [
+  //   trigger('slideIn', [
 
-      transition(':enter', [
-        query('.content', [
-          style({ opacity: 0, transform: 'translateX(-100px)' }),
-          stagger(250, [
-            animate('300ms ease-in-out', style({ opacity: 1, transform: 'none' })),
-          ]),
-        ])
-      ]),
+  //     transition(':enter', [
+  //       query('.content', [
+  //         style({ opacity: 0, transform: 'translateX(-100px)' }),
+  //         stagger(250, [
+  //           animate('300ms ease-in-out', style({ opacity: 1, transform: 'none' })),
+  //         ]),
+  //       ])
+  //     ]),
 
-      transition(':leave', [
-        query('.content', [
-          stagger(250, [
-            animate('300ms ease-in-out', style({ opacity: 0, transform: 'translateX(-100px)' })),
-          ]),
-        ])
-      ])
-    ])
-  ]
+  //     transition(':leave', [
+  //       query('.content', [
+  //         stagger(250, [
+  //           animate('300ms ease-in-out', style({ opacity: 0, transform: 'translateX(-100px)' })),
+  //         ]),
+  //       ])
+  //     ])
+  //   ])
+  // ]
 })
 
 export class HomeComponent implements OnInit {
 
-  currentState;
+  // currentState;
 
-  showHide() {
-    this.currentState = !this.currentState;
-    // console.log(this.currentState);
-  }
+  // showHide() {
+  //   this.currentState = !this.currentState;
+  //   // console.log(this.currentState);
+  // }
 
   constructor() { }
 
