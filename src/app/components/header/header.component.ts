6704031fb6f08771b7335bc32a071ee0bb1cf836
dcocking <dustin.cocking@gmail.com>
2018-11-14 21:38:13 -0500

    Omg I got it working

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -12,78 +12,44 @@ import { MessageBusService } from '../../services/messagebus/messagebus.service'
 
   animations: [
 
-    // trigger('collapseExpand', [
-    //   state('expanded', style({background: 'rgba(0,0,0,0)'})),
-    //   state('collapsed', style({background: 'rgba(0,0,0,0.8)', 'box-shadow': '0 3px 3px -2px rgba(0, 0, 0, .2), 0 3px 4px 0 rgba(0, 0, 0, .14), 0 1px 8px 0 rgba(0, 0, 0, .12)'})),
-    //   transition('collapsed <=> expanded', [animate('300ms ease-in-out')])
-    // ]),
+    trigger('headerScroll', [
+      state('home', style({ background: 'rgba(0,0,0,0)' })),
+      state('away', style({ background: 'rgba(0,0,0,0.8)', 'box-shadow': '0 3px 3px -2px rgba(0, 0, 0, .2), 0 3px 4px 0 rgba(0, 0, 0, .14), 0 1px 8px 0 rgba(0, 0, 0, .12)' })),
+      transition('home <=> away', [
+        query(':self', [
+          animate('300ms ease-in-out')
+        ])
+      ])
+    ]),
 
     trigger('lightningStrike', [
-      transition('off => on', [
-        query(':self', style({ background: 'rgba(0,0,0,0)', 'box-shadow': 'none' })),
-        query('i', style({ opacity: 0, transform: 'translateY(-100px)', color: '#000' })),
-        query(':self', animate('300ms ease-in-out', style({ background: 'rgba(0,0,0,0.8)', 'box-shadow': '0 3px 3px -2px rgba(0, 0, 0, .2), 0 3px 4px 0 rgba(0, 0, 0, .14), 0 1px 8px 0 rgba(0, 0, 0, .12)' }))),
-        query('i', animate(1000, style({ opacity: 1, transform: 'none', color: '#FBDA4E' }))),
-      ]),
+      state('on', style({ 
+        'color': '#FCDC3E',
+         transform: 'translateY(0)'
+      })),
+      state('off', style({
+        'color': '#000',
+        transform: 'translateY(0)'
+      })),
       transition('on => off', [
-        query(':self', style({ background: 'rgba(0,0,0,0.8)', 'box-shadow': '0 3px 3px -2px rgba(0, 0, 0, .2), 0 3px 4px 0 rgba(0, 0, 0, .14), 0 1px 8px 0 rgba(0, 0, 0, .12)' })),
-        query('i', style({ opacity: 1, transform: 'none', color: '#FBDA4E' })),
-        query(':self', animate('300ms ease-in-out', style({ background: 'rgba(0,0,0,0)', 'box-shadow': 'none' }))),
-        query('i', animate(1000, style({ opacity: 1, transform: 'none', color: '#000' }))),
+        style({ 'color': '#FCDC3E'}),
+        query(':self', [
+          animate('300ms ease-in-out', style({
+            'color': '#000',
+          }))
+        ])
       ]),
+      transition('off => on', [
+        style({ 'color': '#000', transform:'translateY(-100px)' }),
+        query(':self', [
+          animate('300ms ease-in-out', style({
+            'color': '#FCDC3E',
+            transform: 'translateY(0px)'
+          }))
+        ])
+      ])
+
     ])
-
-    // trigger('headerScroll', [
-    //   state('home', style({ background: 'rgba(0,0,0,0)' })),
-    //   state('away', style({ background: 'rgba(0,0,0,0.8)', 'box-shadow': '0 3px 3px -2px rgba(0, 0, 0, .2), 0 3px 4px 0 rgba(0, 0, 0, .14), 0 1px 8px 0 rgba(0, 0, 0, .12)' })),
-    //   transition('home <=> away', [
-    //     query(':self', [
-    //       animate('300ms ease-in-out')
-    //     ])
-    //   ])
-    // ]),
-
-  //   trigger('lightningStrike', [
-  //     transition('* => *', [
-  //       query(':self', [
-  //         style({ opacity: 0, transform: 'translateY(-100px)' }),
-  //         stagger(-30, [
-  //           animate('500ms cubic-bezier(0.35, 0, 0.25, 1)', style({ opacity: 1, transform: 'none' }))
-  //         ])
-  //       ])
-  //     ])
-  //   ]),
-  // ]
-
-    //     trigger('pageAnimations', [
-    //   transition(':enter', [
-    //     query('.hero, form', [
-    //       style({opacity: 0, transform: 'translateY(-100px)'}),
-    //       stagger(-30, [
-    //         animate('500ms cubic-bezier(0.35, 0, 0.25, 1)', style({ opacity: 1, transform: 'none' }))
-    //       ])
-    //     ])
-    //   ])
-    // ]),
-
-    // trigger('lightningStrike', [
-    //   state('off', style({ 'color': '#000', transform:'scale(1)'})),
-    //   state('on', style({ 'color': '#FBDA4E', transform:'scale(1.2)'})),
-    //   transition('off <=> on', [animate('300ms ease-in-out')])
-    // ]),
-
-    // trigger('lightning', [
-    //   state('attack', style({color:'#FBDA4E', transform:'translateY(0)'})),
-    //   state('home', style({color:'#000', transform:'translateY(-100)'})),
-    //   transition('attack <=> home', [animate('300ms ease-in-out')])
-    // ])
-
-    // trigger('openClose', [
-    //   state('closed, void', style({height: '0px'})),
-    //   state('opened', style({height: '*'})),
-    //   transition('closed <=> opened', [animate('300ms ease-in-out')]) 
-    // ])  
-
   ]
 })
 
@@ -95,7 +61,7 @@ export class HeaderComponent implements OnInit {
   // lightningState: string = 'home';
 
   headerState: string = 'home';
-  lightningState;
+  lightningState: string = 'off';
 
   constructor(private el: ElementRef, private messageBus: MessageBusService) { }
 
