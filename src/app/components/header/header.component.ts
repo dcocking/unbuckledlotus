f868a2f6d22282286bc80d408bc98d2a482e2d95
dcocking <dustin.cocking@gmail.com>
2018-11-14 22:29:19 -0500

    Animating links

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, HostListener, ElementRef } from '@angular/core';
 import { NgIf } from '@angular/common';
-import { animate, state, style, transition, trigger, keyframes, query, stagger,group } from '@angular/animations';
+import { animate, state, style, transition, trigger, keyframes, query, stagger, group } from '@angular/animations';
 import { MaterialModule } from '../../../material/material.module'
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MessageBusService } from '../../services/messagebus/messagebus.service';
@@ -13,8 +13,13 @@ import { MessageBusService } from '../../services/messagebus/messagebus.service'
   animations: [
 
     trigger('headerScroll', [
-      state('home', style({ background: 'rgba(0,0,0,0)' })),
-      state('away', style({ background: 'rgba(0,0,0,0.8)', 'box-shadow': '0 3px 3px -2px rgba(0, 0, 0, .2), 0 3px 4px 0 rgba(0, 0, 0, .14), 0 1px 8px 0 rgba(0, 0, 0, .12)' })),
+      state('home', style({
+        background: 'rgba(0,0,0,0)'
+      })),
+      state('away', style({
+        background: 'rgba(0,0,0,0.8)',
+        'box-shadow': '0 3px 3px -2px rgba(0, 0, 0, .2), 0 3px 4px 0 rgba(0, 0, 0, .14), 0 1px 8px 0 rgba(0, 0, 0, .12)'
+      })),
       transition('home <=> away', [
         query(':self', [
           animate('300ms ease-in-out')
@@ -23,16 +28,16 @@ import { MessageBusService } from '../../services/messagebus/messagebus.service'
     ]),
 
     trigger('lightningStrike', [
-      state('on', style({ 
+      state('on', style({
         'color': '#FCDC3E',
-         transform: 'translateY(0)'
+        transform: 'translateY(0)'
       })),
       state('off', style({
         'color': '#000',
         transform: 'translateY(0)'
       })),
       transition('on => off', [
-        style({ 'color': '#FCDC3E'}),
+        style({ 'color': '#FCDC3E' }),
         query(':self', [
           animate('300ms ease-in-out', style({
             'color': '#000',
@@ -40,7 +45,7 @@ import { MessageBusService } from '../../services/messagebus/messagebus.service'
         ])
       ]),
       transition('off => on', [
-        style({ 'color': '#000', transform:'translateY(-100px)', opacity: 0}),
+        style({ 'color': '#000', transform: 'translateY(-100px)', opacity: 0 }),
         query(':self', [
           group([
             animate('300ms ease-in', style({
@@ -60,16 +65,25 @@ import { MessageBusService } from '../../services/messagebus/messagebus.service'
         ])
       ])
 
-    ])
+    ]),
+
+    trigger('glowLinks', [
+      state('home', style({
+        'color': '#000'
+      })),
+      state('away', style({
+        'color': '#FCDC3E'
+      })),
+      transition('home <=> away', [
+        query(':self', [
+          animate('300ms ease-in-out', )
+        ])
+      ]),
+    ]),
   ]
 })
 
 export class HeaderComponent implements OnInit {
-
-  // collapseExpand: string = 'expanded';
-  // mobileDrawerState: boolean;  
-  // openClose: string = 'closed';
-  // lightningState: string = 'home';
 
   headerState: string = 'home';
   lightningState: string = 'off';
@@ -91,57 +105,6 @@ export class HeaderComponent implements OnInit {
       console.log(this.headerState, this.lightningState);
     }
   }
-
-  // @HostListener('window:scroll', ['$event'])
-  // checkScroll() {
-  //   const componentPosition = this.el.nativeElement.offsetTop
-  //   const scrollPosition = window.pageYOffset
-  //   if ((scrollPosition == componentPosition) && (this.openClose = 'closed')) {
-  //     this.collapseExpand = 'expanded';
-  //   } else {
-  //     this.collapseExpand = 'collapsed';
-  //     this.openClose = 'closed';
-  //     this.mobileDrawerState = false;
-  //     // console.log('Mobile drawer state is: ' + this.mobileDrawerState);
-  //     // Send message to collapse name and header links
-  //     this.messageBus.event.emit({
-  //       'mobileDrawer': 'false'
-  //     });
-  //   }
-  // }
-
-  // mobileDrawerToggle() {
-  //   const componentPosition = this.el.nativeElement.offsetTop
-  //   const scrollPosition = window.pageYOffset
-
-  //   if ((this.mobileDrawerState) && (scrollPosition == componentPosition)) {
-  //     this.mobileDrawerState = false;
-  //     console.log('Mobile drawer state is: ' + this.mobileDrawerState);
-  //     this.openClose ='closed';
-  //     this.collapseExpand = 'expanded';
-
-  //     // Send message to collapse name and header links
-  //     this.messageBus.event.emit({
-  //       'mobileDrawer': 'false'
-  //     });
-
-  //   } else if ((this.mobileDrawerState) && (scrollPosition != componentPosition)) {
-  //     this.mobileDrawerState = false;
-  //     console.log('Mobile drawer state is: ' + this.mobileDrawerState);
-  //     this.openClose = 'closed';
-
-  //   } else {
-  //     this.mobileDrawerState = true;
-  //     console.log('Mobile drawer state is: ' + this.mobileDrawerState);
-  //     this.openClose = 'opened';
-  //     this.collapseExpand = 'collapsed';
-
-  //     // Send message to expand name and header links
-  //     this.messageBus.event.emit({
-  //       'mobileDrawer': 'true'
-  //     });
-  //   } 
-  // }
 
   ngOnInit() {
   }
